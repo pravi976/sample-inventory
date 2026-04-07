c26249d12ead00a664ec2f3020038dbbc7dc2140
Gantala123 <snehagjava89@gmail.com>
2026-04-07 17:51:13 +0530

    Run regression against live inventory responses

--- src/main/java/com/fedex/sample/inventory/SampleInventoryApplication.java
+++ src/main/java/com/fedex/sample/inventory/SampleInventoryApplication.java
@@ -3,7 +3,7 @@ package com.fedex.sample.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.fedex.sample.inventory", "com.fedex.cdc"})
+@SpringBootApplication
 public class SampleInventoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(SampleInventoryApplication.class, args);
